e53097dacbb7bd2d088eb9a5ccafe329d2b2a9db
SAADTARIQ2503 <saad.tariq2503@gmail.com>
2024-08-06 16:27:30 +0500

    Every thing working

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let text="";
+
+let btnpnl=document.querySelectorAll(`.btnPnl`);
+let txtbx=document.querySelector("#input");
 
 function Add(num1,num2){return num1+num2;}
 function Subtract(num1,num2){return num1-num2;}
@@ -18,18 +22,45 @@ function operate(num1,num2,operator)
             return Divide(num1,num2);
     }
 }
-
-
-let firstNum=0,secondNum=0,operator='';
-let text="";
-
-
-
-let btnpnl=document.querySelector(`.numbers`);
-for(let i=0;i<10;i++)
+function evlauate(str)
 {
-    btn.addEventListener("click",()=>{text+= btn.id
-        console.log(text,btn.id);
+    let firstNum=0,secondNum=0,operator='';
+    let operators="+-*/";
+    for(let i=0;i<str.length;i++)
+    {
+        if(operators.includes(str[i]))
+        {
+            firstNum=Number(str.slice(0,i));
+            secondNum=Number(str.slice(i+1,str.length));
+            operator=str[i];
+            break;
+        }
+    }
+    text=operate(firstNum,secondNum,operator);
+    txtbx.value=text;
+    text="";
+}
+
+
+for(let i=0;i<17;i++)
+{
+    if(i==14)
+    {
+        btnpnl[i].addEventListener("click",()=>{evlauate(text);});
+        continue;
+    }
+    else if(i==16)
+    {
+        btnpnl[i].addEventListener(
+            "click",()=>{
+            text="";
+            txtbx.value=text;}
+        );
+        continue;
+    }
+    btnpnl[i].addEventListener(
+        "click",()=>{text+=  btnpnl[i].id
+        txtbx.value=text;
     });
 }
 // let btnpnl=document.querySelectorAll(".1")
